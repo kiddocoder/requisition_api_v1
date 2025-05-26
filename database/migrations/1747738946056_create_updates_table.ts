619dc1524e428e9e2a55c603111fe9f256131c49
Trésor Ndayikeza <160187183+kiddocoder@users.noreply.github.com>
2025-05-26 15:38:12 +0200

    dfgkjrldfktjg

--- database/migrations/1747738946056_create_updates_table.ts
+++ database/migrations/1747738946056_create_updates_table.ts
@@ -4,14 +4,14 @@ export default class extends BaseSchema {
   protected tableName = 'requisition_items'
 
   async up() {
-    this.schema.alterTable(this.tableName, (table) => {
+    // this.schema.alterTable(this.tableName, (table) => {
     //   //  table.bigInteger('unit_price').defaultTo(0)
 
-      table.boolean('accepted').defaultTo(true)  /* 'direction'*/
+      // table.boolean('accepted').defaultTo(true)  /* 'direction'*/
 
     //   // table.bigInteger('nouveau_prix').defaultTo(0)
 
-    })
+    // })
   }
 
   async down() {
